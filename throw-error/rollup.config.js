ac806c85a21f18992a7e6bcd06cedcd89a557648
Andreas Rozek <A.Rozek@gmx.de>
2021-07-04 18:22:07 +0200

    throw-error build configuration

---
+++ throw-error/rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs   from '@rollup/plugin-commonjs'
+import resolve    from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript';
+//import { terser } from 'rollup-plugin-terser' // uncomment for minification
+
+export default {
+  input: './src/throw-error.ts',
+  output: [
+    {
+      file:     './dist/throw-error.js',
+      format:    'umd',        // builds for both Node.js and Browser
+      name:      'throwError', // required for UMD modules
+      noConflict:true,
+      exports:   'default',
+      sourcemap: true,
+//    plugins: [terser({ format:{ comments:false, safari10:true } })], // dto.
+    },{
+      file:     './dist/throw-error.esm.js',
+      format:   'esm',
+      sourcemap:true,
+    }
+  ],
+  plugins: [
+    resolve(), commonjs(), typescript(),
+  ],
+};
