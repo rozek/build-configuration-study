cb26ee269163666ebb1b7985d795428c9a699040
Andreas Rozek <A.Rozek@gmx.de>
2021-07-04 18:23:18 +0200

    svelte-timer build configuration

---
+++ svelte-timer-action/rollup.config.js
@@ -0,0 +1,16 @@
+import commonjs   from '@rollup/plugin-commonjs'
+import resolve    from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: './src/svelte-timer-action.ts',
+  external:['throw-error'],                  // list of (unbundled) dependencies
+  output: {
+    file:     './dist/svelte-timer-action.esm.js',
+    format:   'esm',
+    sourcemap:true,
+  },
+  plugins: [
+    resolve({ browser:true, dedupe:['svelte'] }), commonjs(), typescript(),
+  ],
+};
