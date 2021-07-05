650a9461e6cf57ec09f6ea05d47a7a5e9a8a4156
Andreas Rozek <A.Rozek@gmx.de>
2021-07-05 07:20:23 +0200

    update build config

--- throw-error/rollup.config.js
+++ throw-error/rollup.config.js
@@ -1,5 +1,3 @@
-import commonjs   from '@rollup/plugin-commonjs'
-import resolve    from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 //import { terser } from 'rollup-plugin-terser' // uncomment for minification
 
@@ -21,6 +19,6 @@ export default {
     }
   ],
   plugins: [
-    resolve(), commonjs(), typescript(),
+    typescript(),
   ],
 }
